d5dbc903d1501cc39ac3c09ad44c6b97472061db
Hacker-Boy9999 <83418310+Hacker-Boy9999@users.noreply.github.com>
2021-05-03 20:11:14 +0300

    Create Find Jack.py

---
+++ Find Jack.py
@@ -0,0 +1,42 @@
+import time
+
+print("You and jack are going on an adventure!")
+time.sleep(2)
+print("Suddenly you hear a sound...")
+time.sleep(2)
+print("The sound is coming from behind you.")
+time.sleep(2)
+print("You look behind...")
+time.sleep(2)
+print("There is no one there")
+time.sleep(2)
+print("You look at jack and...")
+time.sleep(2)
+print("He is gone")
+time.sleep(2)
+print("You look for him but he is no where to be seen.")
+time.sleep(2)
+print("Until you see a house")
+time.sleep(2)
+print("You decide to go in...")
+print("you entered a room full of tigers, but they are sleeping")
+i2 = input("type 9 to get past the room: ")
+if i2 == "9":
+        i3 = input("This is strange... What is 1 + 1: ")
+        if i3 == "2":
+            print("Lol, you died")
+            time.sleep(2)
+        if i3 == "11":
+            print("Give this man a scholar")
+            time.sleep(2)
+            i4 = input("What is 2 + 2: ")
+            if i4 == "4":
+                print("Noice, you survived.")
+                time.sleep(2)
+                print("You rescued Jack and went home")
+                time.sleep(2)
+            if i4 == "22":
+                print("Go to KG")
+                time.sleep(2)
+                print("Btw, you died")
+                time.sleep(2)
